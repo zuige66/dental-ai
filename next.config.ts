6c5f94b6dd6ac59bf9c56c4b64023a3de0ee555a
zuige66 <liruisen2007@163.com>
2025-10-31 00:33:14 +0800

    next

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true, // 让 GitHub Pages 能显示图片
+  },
+  basePath: '/dental-ai', // 👈 必须加上你的仓库名
+  assetPrefix: '/dental-ai/', // 👈 静态资源路径前缀
 };
 
 export default nextConfig;
