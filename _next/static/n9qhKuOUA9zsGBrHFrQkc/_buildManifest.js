b275cd47eea8cf8fc8268221254d3f82a1d776f3
zuige66 <liruisen2007@163.com>
2025-10-31 08:59:52 +0800

    Updates

--- _next/static/n9qhKuOUA9zsGBrHFrQkc/_buildManifest.js
+++ _next/static/n9qhKuOUA9zsGBrHFrQkc/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/dental-ai//_next/:path+",
+        "destination": "/dental-ai/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
